b8b7f20699914493ee138a3d85a890458cb1bc30
andrewwitte97 <wittea91@my.sunyulster.edu>
2019-11-07 08:42:55 -0500

    Friends page need to be set up

--- client/fitness_tracker/src/router/index.js
+++ client/fitness_tracker/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Friends from '../views/Friends.vue'
+//import Friends from '../views/Friends.vue'
 
 
 Vue.use(VueRouter)
